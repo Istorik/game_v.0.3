4a2c996b9edc58096df8e39eb0fa11d09c45ca92
Istorik <tros2002@list.ru>
2018-07-17 03:15:30 +0300

    fix #156 Технически, доска объявлений созадана

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from news.models import News
 
-# Create your views here.
+
+
+def news_list(request):
+    """ Вывод списка всех квестов
+    """
+    news = News.objects.all()
+    return render(request, "news/news_list.html", {"news":news})
+
+def new_single(request, pk):
+    """ Вывод выбранного квеста
+    """
+    new = get_object_or_404(News, id=pk)
+    return render(request, "news/new_single.html", {"new": new})
